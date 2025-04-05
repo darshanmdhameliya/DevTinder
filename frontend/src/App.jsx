2c934982f424a8dcedef2478f1f9246b6f72c9b8
Darshan Dhameliya <skillqodedarshan@gmail.com>
2025-04-05 09:48:53 +0530

    Routing

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import Body from "./Body";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./Login";
+import Profile from "./Profile";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
